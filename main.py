20ae8a2f4dccb94a5048298d56b065293c161136
wiktorsk8 <wikto4k@gmail.com>
2023-02-22 12:15:19 +0100

    main file added

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def power(a, N):
+    if N == 0:
+        return 1
+    if N % 2 == 0:
+        x = power(a, N//2)
+        return x*x
+    else:
+        return a * power(a, N-1)
+
+print(power(3,4))
